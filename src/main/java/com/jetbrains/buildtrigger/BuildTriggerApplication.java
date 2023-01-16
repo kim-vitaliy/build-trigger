79b9f5d3ba0017dec6ac96080c5f28e6d20924ad
Vitaliy Kim <sib.greit@yandex.ru>
2023-01-16 19:05:27 +0300

    Initial

---
+++ src/main/java/com/jetbrains/buildtrigger/BuildTriggerApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.buildtrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTriggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildTriggerApplication.class, args);
+    }
+
+}
